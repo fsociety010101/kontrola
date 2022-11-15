b40511b793837995cfc1e83d9c6d499f59d04c42
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-11-15 18:15:42 +0100

    Punkt

---
+++ Punkt.py
@@ -0,0 +1,9 @@
+# 3d point class in
+class Punkt:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return "x: " + str(self.x) + " y: " + str(self.y) + " z: " + str(self.z)
