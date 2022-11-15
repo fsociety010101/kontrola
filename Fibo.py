34c43d37aa290518aed259bf231449ac0f2f6b41
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-11-15 23:24:52 +0100

    Fibor

--- Fibo.py
+++ Fibo.py
@@ -1,9 +1,19 @@
 class Fibo:
     def __init__(self, n):
-        self.n = n
-        self.fib = [0,1]
-        for i in range(2,n+1):
-            self.fib.append(self.fib[i-1] + self.fib[i-2])
+        self.fib_num = self.fibo_rec(n)
+
+    # fibonacci recursive   
+    def fibo_rec(self, n):
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fibo_rec(n-1) + self.fibo_rec(n-2)
 
     def get_fib(self):
-        return self.fib
+        return self.fib_num
+
+a = Fibo(10)
+print(a.get_fib())
+
