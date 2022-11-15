7829e66846b1aeedda6dcebb491dd8020f5d158a
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2022-11-15 23:31:29 +0100

    Kolejny

--- Nonconf.py
+++ Nonconf.py
@@ -4,7 +4,7 @@ import Fibo
 
 print("Pierwszy feature")
 
-print("Drugi feature")
+print("Kolejny feature")
 punkt1 = Punkt.Punkt(0, 0, 0)
 punkt2 = Punkt.Punkt(1, 2, 3)
 print("Distance: ", punkt1.distance(punkt2))
@@ -12,7 +12,7 @@ print("Distance: ", punkt1.distance(punkt2))
 fibo = Fibo.Fibo(13)
 print(fibo.get_fib())
 
-fibo_feature = Fibo.Fibo(10)
+fibo_feature = Fibo.Fibo(12)
 print(fibo_feature.get_fib())
 
 print("Trzeci feature")
